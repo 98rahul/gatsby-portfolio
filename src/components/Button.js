eaf6b52169aab1dbc8b2207e9bd1041801a6b715
Rahul Raj <rahulraj94316@gmail.com>
2023-04-05 01:23:52 +0530

    buttons-all completed

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,16 @@
 import React from "react";
 import * as styles from "../styles/Button.module.css";
-const Button = ({ title, btnType, key }) => {
-  console.log(btnType);
+const Button = ({ title, type, outline, rounded }) => {
+  console.log(title, type, outline, rounded);
   return (
-    <div className={`${styles['btn']} ${styles[btnType]}`} key={key}>
-      <button >{title}</button>
+    <div
+      className={`${styles["btn"]} ${styles[type]} ${
+        rounded ? `${styles.rounded}` : ""
+      }  ${outline ? `${styles.outline}` : ""}
+      ${outline && rounded ? `${styles.roundOutline}` : ""}
+      `}
+    >
+      {title}
       {/* <input value={title} readOnly /> */}
     </div>
   );
